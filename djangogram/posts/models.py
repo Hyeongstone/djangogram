c9cc13ce68034eba8b5fea40dbd695b7dfb1f301
Hyeongstone <dlgudtjr4806@naver.com>
2020-06-23 21:25:00 +0900

    Make dateModel(Table)

---
+++ djangogram/posts/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from djangogram.users import models as user_model
+
+# Create your models here.
+
+# 클래스마다 만들어주기 번거로우니 따로 만들어 상속
+# 데이터 생성날짜와 업데이트 날짜
+class TimeStampedModel(models.Model):
+    create_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now_add = True)
+    
+    # Meta를 만들 시 TimeStampedModel은 단독으로 테이블 생성 X
+    class Meta:
+        abstract = True
+
+class Post(TimeStampedModel):
+    author = models.ForeignKey(
+                user_model.User,
+                null = True,
+                #on_delete = 외래키를 갖는 USER가 사라지면 어떻게 처리될것인가
+                on_delete=models.CASCADE,
+                related_name = 'post_author'
+            )
+    image = models.ImageField(blank = True)
+    caption = models.TextField(blank = True)
+    image_likes = models.ManyToManyField(
+                    user_model.User,
+                    related_name = 'post_image_likes'
+                )
+
+
+class Comment(TimeStampedModel):
+    author = models.ForeignKey(
+            user_model.User,
+            null = True,
+            #on_delete = 외래키를 갖는 USER가 사라지면 어떻게 처리될것인가
+            on_delete=models.CASCADE,
+            related_name = 'post_author'
+        )
+    posts = models.ForeignKey(
+            Post,
+            null = True,
+            #on_delete = 외래키를 갖는 USER가 사라지면 어떻게 처리될것인가
+            on_delete=models.CASCADE,
+            related_name = 'comment_post'
+        )
+    contents = models.TextField(blank=True)
