a39ca44cb50409b1fcf2bce336af2bf79f308a57
Hyeongstone <dlgudtjr4806@naver.com>
2020-06-26 15:58:35 +0900

    signup page

--- djangogram/users/forms.py
+++ djangogram/users/forms.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import forms, get_user_model
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
+from django import forms as django_form
+
 
 User = get_user_model()
 
@@ -28,3 +30,28 @@ class UserCreationForm(forms.UserCreationForm):
             return username
 
         raise ValidationError(self.error_messages["duplicate_username"])
+
+
+class SignUpForm(django_form.ModelForm):
+    class Meta:
+        model = User
+        fields = ['email','name','username','password']
+
+        labels = {
+            'email' : '이메일 주소',
+            'name' : '이름',
+            'username' : '사용자 이름',
+            'password' : '비밀번호',
+        }
+
+        widgets = {
+            'password' : django_form.PasswordInput(),
+        }
+
+    # 패스워드가 유효하지 않아 기존의 save함수를 오버라이딩(재정의) 
+    def save(self, commit=True):    
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        user.save()
+
+        return user
